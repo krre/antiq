5ec4da7f27d7a4516604f9b18e5a56f0c159e99b
Vladimir Zarypov <krre31@gmail.com>
2015-07-10 13:05:15 +0300

    Debug class is not singleton

--- src/core/debug.cpp
+++ src/core/debug.cpp
@@ -2,20 +2,8 @@
 
 BEGIN_NAMESPACE_A3D
 
-Debug *Debug::m_instance = nullptr;
-
 Debug::Debug()
 {
-    std::cout << "debug" << std::endl;
-}
-
-Debug &Debug::instance()
-{
-    if (m_instance == nullptr) {
-        m_instance = new Debug();
-    }
-
-    return *m_instance;
 }
 
 END_NAMESPACE_A3D
--- src/core/debug.h
+++ src/core/debug.h
@@ -4,13 +4,13 @@
 
 BEGIN_NAMESPACE_A3D
 
-#define DBG a3d::Debug::instance()
+#define DBG a3d::Debug()
 
-// Singleton
 class Debug
 {
 public:
-    static Debug &instance();
+    Debug();
+    ~Debug() { std::cout << std::endl; }
     inline Debug &operator<<(bool t) { std::cout << (t ? "true" : "false") << " "; return *this; }
     inline Debug &operator<<(int t) { std::cout << t << " "; return *this; }
     inline Debug &operator<<(float t) { std::cout << t << " "; return *this; }
@@ -18,9 +18,6 @@ public:
     inline Debug &operator<<(const char *t) { std::cout << t << " "; return *this; }
     inline Debug &operator<<(void *t) { std::cout << t << " "; return *this; }
     inline Debug &operator<<(std::nullptr_t) { std::cout << "(nullPtr)" << " "; return *this; }
-private:
-    Debug();
-    static Debug *m_instance;
 };
 
 END_NAMESPACE_A3D
