c48a9ce848487a296203c4b13a5f8032819ad1e8
Vladimir Zarypov <krre31@gmail.com>
2015-07-25 18:12:36 +0300

    refactored command line tool

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    if (argc > 1) {
+    if (argc != 2) {
+        std::cout << "AngieLogic Interpreter" << std::endl;
+        std::cout << "Usage: al [sourcefile]" << std::endl;
+
+    } else {
         std::string filePath = argv[1];
         Program program(filePath);
         program.compile();
         program.run();
-    } else {
-        std::cout << "AngieLogic Interpreter" << std::endl;
-        std::cout << "Usage: al file" << std::endl;
     }
 }
