36bf4bae3a2d4844693564bf5fb60700852616d3
Vladimir Zarypov <krre31@gmail.com>
2021-05-18 18:35:01 +0300

    Fix location function for application name

--- src/entity/application.rs
+++ src/entity/application.rs
@@ -9,19 +9,19 @@ impl Application {
     pub fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     }
+
+    pub fn name() -> Option<String> {
+        std::env::current_exe()
+            .ok()?
+            .file_name()?
+            .to_str()?
+            .to_owned()
+            .into()
+    }
 }
 
 impl Entity for Application {
     fn id(&self) -> Id {
         self.id
     }
-}
-
-pub fn name() -> Option<String> {
-    std::env::current_exe()
-        .ok()?
-        .file_name()?
-        .to_str()?
-        .to_owned()
-        .into()
 }
