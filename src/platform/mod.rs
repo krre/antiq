b25df54cc1582ade79227f6f35ce5c7942e03c69
Vladimir Zarypov <krre31@gmail.com>
2021-06-19 18:35:04 +0300

    Fix platform condition compilation

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,12 +1,17 @@
 #[cfg(target_os = "linux")]
 mod linux;
+#[cfg(target_os = "linux")]
 pub use linux::*;
 
 #[cfg(target_os = "windows")]
 mod windows;
+#[cfg(target_os = "windows")]
+pub use windows::*;
 
 #[cfg(target_os = "macos")]
 mod macos;
+#[cfg(target_os = "macos")]
+pub use macos::*;
 
 pub trait PlatformWindow {
     fn set_title(&mut self, title: &str);
