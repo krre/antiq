c38d9ab04f778a9bbc00d241e440acc5ead57788
Vladimir Zarypov <krre31@gmail.com>
2021-06-19 15:59:51 +0300

    Add `Window` constructor

--- src/widget/window.rs
+++ src/widget/window.rs
@@ -6,6 +6,18 @@ pub struct Window {
     id: Id,
 }
 
+impl Window {
+    pub fn new() -> Self {
+        Self { id: Id::default() }
+    }
+}
+
+impl Default for Window {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Entity for Window {
     fn id(&self) -> Id {
         self.id
