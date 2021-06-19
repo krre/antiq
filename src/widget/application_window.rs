d153306ce419c976017af5e204eaa77776626d1a
Vladimir Zarypov <krre31@gmail.com>
2021-06-19 16:01:48 +0300

    Add `ApplicationWindow` constructor

--- src/widget/application_window.rs
+++ src/widget/application_window.rs
@@ -7,6 +7,20 @@ pub struct ApplicationWindow {
     window: Window,
 }
 
+impl ApplicationWindow {
+    pub fn new() -> Self {
+        Self {
+            window: Window::default(),
+        }
+    }
+}
+
+impl Default for ApplicationWindow {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Entity for ApplicationWindow {
     fn id(&self) -> Id {
         self.window.id()
