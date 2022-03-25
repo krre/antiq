17ffde9e0a1f152966a1c73b043d7c2f11fa200b
Vladimir Zarypov <krre31@gmail.com>
2022-03-25 15:54:21 +0200

    Use application window and simple window in multi-window test

--- examples/multi-window.rs
+++ examples/multi-window.rs
@@ -1,20 +1,20 @@
 use antiq::entity::Application;
-use antiq::widget::ApplicationWindow;
+use antiq::widget::{ApplicationWindow, Window};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Multi-window test");
 
     let mut app = Application::new();
 
-    let mut window_1 = ApplicationWindow::new(&mut app)?;
-    window_1.set_title("Window 1");
-    window_1.set_visible(true);
-    app.add_window(window_1);
+    let mut app_window = ApplicationWindow::new(&mut app)?;
+    app_window.set_title("Applicatin Window");
+    app_window.set_visible(true);
+    app.add_window(app_window);
 
-    let mut window_2 = ApplicationWindow::new(&mut app)?;
-    window_2.set_title("Window 2");
-    window_2.set_visible(true);
-    app.add_window(window_2);
+    let mut window = Window::new(&mut app)?;
+    window.set_title("Window");
+    window.set_visible(true);
+    app.add_window(window);
 
     app.run()?;
 
