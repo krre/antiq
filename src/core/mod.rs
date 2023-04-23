c33a91f3b4c0ffb2c50d86464c86685abfe22129
Vladimir Zarypov <krre31@gmail.com>
2023-04-23 15:48:35 +0300

    Remove `widget_core` module

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -6,11 +6,9 @@ mod application;
 mod color;
 mod entity_core;
 mod id;
-mod widget_core;
 
 pub use application::Application;
 pub use color::Color;
 pub use entity_core::EntityCore;
 pub use id::Id;
-pub use widget_core::WidgetCore;
 pub use window::Window;
