999da31c57647687b2f6db600cf5aeab72d3aa08
Vladimir Zarypov <krre31@gmail.com>
2023-04-23 15:49:15 +0300

    Remove `entity_core` module

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -4,11 +4,9 @@ pub mod window;
 
 mod application;
 mod color;
-mod entity_core;
 mod id;
 
 pub use application::Application;
 pub use color::Color;
-pub use entity_core::EntityCore;
 pub use id::Id;
 pub use window::Window;
