7eea926be3dae7a1079a15f567a9285743a5a763
Vladimir Zarypov <krre31@gmail.com>
2023-04-23 15:40:58 +0300

    Clean up `widget` module

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,30 +1,3 @@
-use crate::core::{EntityCore, WidgetCore};
-use core::fmt::Debug;
-
 pub trait Widget {
-    fn widget_ref(&self) -> &WidgetCore;
-
-    fn widget_mut(&mut self) -> &mut WidgetCore;
-
-    fn entity_ref(&self) -> &EntityCore {
-        self.widget_ref().entity_ref()
-    }
-
-    fn entity_mut(&mut self) -> &mut EntityCore {
-        self.widget_mut().entity_mut()
-    }
-
     fn draw(&self);
-}
-
-pub trait WindowWidget {
-    fn id(&self) -> winit::window::WindowId;
-
-    fn render(&self);
-}
-
-impl Debug for dyn WindowWidget {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "WindowWidget")
-    }
 }
