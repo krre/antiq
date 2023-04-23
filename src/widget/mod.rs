6a47a2c5a8de1253a3da6744c357d205285d3e72
Vladimir Zarypov <krre31@gmail.com>
2023-04-23 15:45:19 +0300

    Add `EmptyWidget` struct

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,3 +1,15 @@
 pub trait Widget {
     fn draw(&self);
 }
+
+pub struct EmptyWidget {}
+
+impl EmptyWidget {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Widget for EmptyWidget {
+    fn draw(&self) {}
+}
