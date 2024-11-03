685c03ae74d5a350e439377f2f55e7354d268a8d
Vladimir Zarypov <krre31@gmail.com>
2024-11-03 16:45:36 +0200

    Fix compile errors

--- src/gfx/pipeline_storage.rs
+++ src/gfx/pipeline_storage.rs
@@ -61,13 +61,13 @@ impl PipelineStorage {
             layout: Some(&pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &shader,
-                entry_point: "vs_main",
+                entry_point: Some("vs_main"),
                 buffers: &[],
                 compilation_options: Default::default(),
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
-                entry_point: "fs_main",
+                entry_point: Some("fs_main"),
                 targets: &[Some(swapchain_format.into())],
                 compilation_options: Default::default(),
             }),
