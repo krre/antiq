0d93e6bb1df191491a00b23996bad2c1bdefe3e5
Vladimir Zarypov <krre31@gmail.com>
2024-12-04 09:50:05 +0200

    Rename `EventLoop::inner` member to `platform_event_loop`

--- src/core/event_loop.rs
+++ src/core/event_loop.rs
@@ -1,16 +1,16 @@
 use crate::platform;
 pub struct EventLoop {
-    inner: Box<dyn platform::PlatformEventLoop>,
+    platform_event_loop: Box<dyn platform::PlatformEventLoop>,
 }
 
 impl EventLoop {
     pub fn new() -> Self {
         Self {
-            inner: Box::new(platform::EventLoop::new()),
+            platform_event_loop: Box::new(platform::EventLoop::new()),
         }
     }
 
     pub fn run(&self) {
-        self.inner.run();
+        self.platform_event_loop.run();
     }
 }
