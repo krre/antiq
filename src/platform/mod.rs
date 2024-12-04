4e6f16e0bacd08db06c391bb26390218b8176ba1
Vladimir Zarypov <krre31@gmail.com>
2024-12-04 07:46:42 +0200

    Use `cfg_if` macros for separate platform specific code in `platform` module

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,8 +1,17 @@
-#[cfg(linux)]
-pub mod linux;
-
-#[cfg(macos)]
-pub mod macos;
-
-#[cfg(windows)]
-pub mod windows;
+cfg_if::cfg_if! {
+    if  #[cfg(linux)] {
+        pub mod linux;
+        #[allow(unused_imports)]
+        pub use linux::*;
+    } else if #[cfg(macos)] {
+        pub mod macos;
+        #[allow(unused_imports)]
+        pub use macos::*;
+    } else if #[cfg(windows)] {
+        pub mod windows;
+        #[allow(unused_imports)]
+        pub use windows::*;
+    } else {
+        compile_error!("Platform not supported")
+    }
+}
