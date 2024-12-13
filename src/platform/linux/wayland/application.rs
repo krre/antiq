a0ed8a46267020a19ecdefd95b47bc3805d3b948
Vladimir Zarypov <krre31@gmail.com>
2024-12-13 17:54:55 +0200

    Refactor creating Wayland connection

--- src/platform/linux/wayland/application.rs
+++ src/platform/linux/wayland/application.rs
@@ -1,16 +1,20 @@
 use std::rc::Rc;
+use wayland_client::Connection;
 
 use crate::platform::PlatformApplication;
 
 pub struct Application {
-    connection: Rc<wayland_client::Connection>,
+    connection: Rc<Connection>,
 }
 
 impl Application {
     pub fn new() -> Result<Box<dyn PlatformApplication>, Box<dyn std::error::Error>> {
-        let connection = Rc::new(wayland_client::Connection::connect_to_env()?);
-
+        let connection = Rc::new(Connection::connect_to_env()?);
         Ok(Box::new(Self { connection }))
+    }
+
+    pub fn connection(&self) -> Rc<Connection> {
+        self.connection.clone()
     }
 }
 
