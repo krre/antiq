b10bee7867e600d981d94d926c8024efb0513f9c
Vladimir Zarypov <krre31@gmail.com>
2025-01-10 08:27:10 +0200

    Add loading and saving preferences

--- src/core/preferences.rs
+++ src/core/preferences.rs
@@ -1,4 +1,9 @@
-use std::rc::Rc;
+use std::{
+    fs::{DirBuilder, File},
+    io::{Read, Write},
+    path::PathBuf,
+    rc::Rc,
+};
 
 use serde::{Deserialize, Serialize};
 
@@ -31,5 +36,45 @@ impl<T: SD> Preferences<T> {
 
     pub fn get_mut(&mut self) -> &mut T {
         &mut self.data
+    }
+
+    pub fn load(&mut self) {
+        if let Ok(mut file) = File::open(self.path()) {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).unwrap();
+            self.data = serde_json::from_slice::<T>(contents.as_bytes()).unwrap();
+            self.is_loaded = true;
+        }
+
+        self.is_loaded = false;
+    }
+
+    pub fn save(&self) {
+        let value = serde_json::to_string(&self.data).unwrap();
+
+        DirBuilder::new()
+            .recursive(true)
+            .create(self.dir())
+            .unwrap();
+
+        let mut file = File::create(self.path()).unwrap();
+        file.write_all(value.as_bytes()).unwrap();
+    }
+
+    pub fn dir(&self) -> PathBuf {
+        [
+            dirs::config_dir().unwrap(),
+            self.context.organization.clone().into(),
+        ]
+        .iter()
+        .collect()
+    }
+
+    pub fn path(&self) -> PathBuf {
+        let mut result: PathBuf = [self.dir(), self.context.name.clone().into()]
+            .iter()
+            .collect();
+        result.set_extension("prefs");
+        result
     }
 }
