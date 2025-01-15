2c3e331eedbd3d5fa3eaa7f22f67ef349aef0728
Vladimir Zarypov <krre31@gmail.com>
2025-01-15 08:58:12 +0200

    Remove setting visible to true from window examples

--- examples/multi_window.rs
+++ examples/multi_window.rs
@@ -14,7 +14,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     {
         let w = window_1.upgrade().unwrap();
         w.set_title("Multi Window 1");
-        w.set_visible(true);
     }
 
     let window_2 = Window::new(app.context())?;
@@ -22,7 +21,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     {
         let w = window_2.upgrade().unwrap();
         w.set_title("Multi Window 2");
-        w.set_visible(true);
         w.set_position(Pos2D::new(500, 200));
         w.set_size(Size2D::new(300, 300));
         w.set_color(Color::new(1.0, 0.0, 0.0));
