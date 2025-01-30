c23714edcf7a5efdbdd28089916c811355ab6fba
Vladimir Zarypov <krre31@gmail.com>
2025-01-30 17:15:32 +0200

    Remove unused imports

--- src/core/event_loop.rs
+++ src/core/event_loop.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-
 use crate::platform;
 
 use super::{
--- src/core/layout/mod.rs
+++ src/core/layout/mod.rs
@@ -2,10 +2,6 @@ mod column;
 mod grid;
 mod row;
 
-pub use column::Column;
-pub use grid::Grid;
-pub use row::Row;
-
 pub trait Layout {
     fn build(&self);
 }
