2b72a03eb92447c8fa1259d5441659f12247611a
Vladimir Zarypov <krre31@gmail.com>
2025-02-07 17:54:50 +0200

    Add shared memory to Wayland platform application

--- src/platform/linux/wayland/application.rs
+++ src/platform/linux/wayland/application.rs
@@ -1,6 +1,6 @@
 use std::{any::Any, rc::Rc, sync::Arc};
 use wayland_client::{
-    delegate_noop, globals::{registry_queue_init, GlobalListContents}, protocol::{wl_compositor::WlCompositor, wl_registry::{self, WlRegistry}}, Connection, QueueHandle
+    delegate_noop, globals::{registry_queue_init, GlobalListContents}, protocol::{wl_compositor::WlCompositor, wl_registry::{self, WlRegistry}, wl_shm::WlShm}, Connection, QueueHandle
 };
 
 use crate::platform::{PlatformApplication, PlatformEventLoop};
@@ -8,19 +8,23 @@ use crate::platform::{PlatformApplication, PlatformEventLoop};
 pub struct Application {
     pub(crate) connection: Arc<Connection>,
     pub(crate) compositor: WlCompositor,
+    pub(crate) shm: WlShm
 }
 
 struct State {}
 
 delegate_noop!(State: ignore WlCompositor);
+delegate_noop!(State: ignore WlShm);
 
 impl Application {
     pub fn new() -> Result<Rc<dyn PlatformApplication>, Box<dyn std::error::Error>> {
         let connection = Arc::new(Connection::connect_to_env()?);
         let (globals, queue) = registry_queue_init::<State>(&connection).unwrap();
-        let compositor: WlCompositor = globals.bind(&queue.handle(), 4..=5, ()).unwrap();
+        let qh = queue.handle();
+        let compositor: WlCompositor = globals.bind(&qh, 4..=5, ()).unwrap();
+        let shm: WlShm = globals.bind(&qh, 1..=1, ()).unwrap();
 
-        Ok(Rc::new(Self { connection, compositor }))
+        Ok(Rc::new(Self { connection, compositor, shm }))
     }
 
     pub fn connection(&self) -> Arc<Connection> {
