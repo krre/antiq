dd4841a29bd4ac8962d3f6da60842d85f16e8908
Vladimir Zarypov <krre31@gmail.com>
2025-02-07 17:48:50 +0200

    Add compositor to Wayland platform application

--- src/platform/linux/wayland/application.rs
+++ src/platform/linux/wayland/application.rs
@@ -1,33 +1,26 @@
-use std::{any::Any, os::fd::OwnedFd, rc::Rc, sync::Arc};
-use wayland_backend::client::Backend;
+use std::{any::Any, rc::Rc, sync::Arc};
 use wayland_client::{
-    backend,
-    protocol::{wl_display, wl_registry},
-    Connection, Proxy,
+    delegate_noop, globals::{registry_queue_init, GlobalListContents}, protocol::{wl_compositor::WlCompositor, wl_registry::{self, WlRegistry}}, Connection, QueueHandle
 };
 
 use crate::platform::{PlatformApplication, PlatformEventLoop};
 
 pub struct Application {
     pub(crate) connection: Arc<Connection>,
-    pub(crate) registry: wl_registry::WlRegistry
+    pub(crate) compositor: WlCompositor,
 }
 
-struct RegistryData(Arc<Connection>);
+struct State {}
+
+delegate_noop!(State: ignore WlCompositor);
 
 impl Application {
     pub fn new() -> Result<Rc<dyn PlatformApplication>, Box<dyn std::error::Error>> {
         let connection = Arc::new(Connection::connect_to_env()?);
-        let display = connection.display();
-        let registry_data = Arc::new(RegistryData(connection.clone()));
+        let (globals, queue) = registry_queue_init::<State>(&connection).unwrap();
+        let compositor: WlCompositor = globals.bind(&queue.handle(), 4..=5, ()).unwrap();
 
-        let registry: wl_registry::WlRegistry = display
-            .send_constructor(wl_display::Request::GetRegistry {}, registry_data.clone())
-            .unwrap();
-
-        connection.roundtrip().unwrap();
-
-        Ok(Rc::new(Self { connection, registry }))
+        Ok(Rc::new(Self { connection, compositor }))
     }
 
     pub fn connection(&self) -> Arc<Connection> {
@@ -43,31 +36,15 @@ impl PlatformApplication for Application {
     fn init(&self, event_loop: Rc<dyn PlatformEventLoop>) {}
 }
 
-impl backend::ObjectData for RegistryData {
+impl wayland_client::Dispatch<WlRegistry, GlobalListContents> for State {
     fn event(
-        self: Arc<Self>,
-        _: &Backend,
-        msg: backend::protocol::Message<backend::ObjectId, OwnedFd>,
-    ) -> Option<Arc<dyn backend::ObjectData>> {
-        // Here, we parse the wire message into an event using Proxy::parse_event.
-        let (_registry, event) = wl_registry::WlRegistry::parse_event(&self.0, msg).unwrap();
+        state: &mut State,
+        proxy: &WlRegistry,
+        event: wl_registry::Event,
+        data: &GlobalListContents,
+        conn: &Connection,
+        qhandle: &QueueHandle<State>,
+    ) {
 
-        // Similar to the dispatch example, we only care about the global event and
-        // will print out the received globals.
-        if let wl_registry::Event::Global {
-            name,
-            interface,
-            version,
-        } = event
-        {
-            println!("[{}] {} (v{})", name, interface, version);
-        }
-        None
-    }
-
-    // This method is called whenever the object is destroyed. In the case of our registry,
-    // however, there is no way to destroy it, so we will mark it as unreachable.
-    fn destroyed(&self, _: wayland_backend::client::ObjectId) {
-        unreachable!();
     }
 }
