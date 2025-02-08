24b82aada3b944b8d4d8ad87b44a2aa7c295042f
Vladimir Zarypov <krre31@gmail.com>
2025-02-08 11:42:43 +0200

    Add xdg_wm_base to Wayland platform application

--- src/platform/linux/wayland/application.rs
+++ src/platform/linux/wayland/application.rs
@@ -2,19 +2,22 @@ use std::{any::Any, rc::Rc, sync::Arc};
 use wayland_client::{
     delegate_noop, globals::{registry_queue_init, GlobalListContents}, protocol::{wl_compositor::WlCompositor, wl_registry::{self, WlRegistry}, wl_shm::WlShm}, Connection, QueueHandle
 };
+use wayland_protocols::xdg::shell::client::xdg_wm_base::XdgWmBase;
 
 use crate::platform::{PlatformApplication, PlatformEventLoop};
 
 pub struct Application {
     pub(crate) connection: Arc<Connection>,
     pub(crate) compositor: WlCompositor,
-    pub(crate) shm: WlShm
+    pub(crate) shm: WlShm,
+    pub(crate) xdg_wm_base: XdgWmBase
 }
 
 struct State {}
 
 delegate_noop!(State: ignore WlCompositor);
 delegate_noop!(State: ignore WlShm);
+delegate_noop!(State: ignore XdgWmBase);
 
 impl Application {
     pub fn new() -> Result<Rc<dyn PlatformApplication>, Box<dyn std::error::Error>> {
@@ -23,8 +26,9 @@ impl Application {
         let qh = queue.handle();
         let compositor: WlCompositor = globals.bind(&qh, 4..=5, ()).unwrap();
         let shm: WlShm = globals.bind(&qh, 1..=1, ()).unwrap();
+        let xdg_wm_base: XdgWmBase = globals.bind(&qh, 5..=6, ()).unwrap();
 
-        Ok(Rc::new(Self { connection, compositor, shm }))
+        Ok(Rc::new(Self { connection, compositor, shm, xdg_wm_base }))
     }
 
     pub fn connection(&self) -> Arc<Connection> {
