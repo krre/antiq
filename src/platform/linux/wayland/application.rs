1087474764a92ae1b0c169af088e48a670879adf
Vladimir Zarypov <krre31@gmail.com>
2025-02-22 16:47:52 +0200

    Add decoration manager to Wayland platform application

--- src/platform/linux/wayland/application.rs
+++ src/platform/linux/wayland/application.rs
@@ -8,6 +8,7 @@ use wayland_client::{
         wl_shm::WlShm,
     },
 };
+use wayland_protocols::xdg::decoration::zv1::client::zxdg_decoration_manager_v1::ZxdgDecorationManagerV1;
 
 use crate::platform::{PlatformApplication, PlatformEventLoop};
 
@@ -16,12 +17,14 @@ pub struct Application {
     pub(crate) globals: GlobalList,
     pub(crate) compositor: WlCompositor,
     pub(crate) shm: WlShm,
+    pub(crate) xdg_decoration_manager: ZxdgDecorationManagerV1
 }
 
 struct State {}
 
 delegate_noop!(State: ignore WlCompositor);
 delegate_noop!(State: ignore WlShm);
+delegate_noop!(State: ignore ZxdgDecorationManagerV1);
 
 impl Application {
     pub fn new() -> Result<Rc<dyn PlatformApplication>, Box<dyn std::error::Error>> {
@@ -30,12 +33,14 @@ impl Application {
         let qh = queue.handle();
         let compositor: WlCompositor = globals.bind(&qh, 4..=5, ()).unwrap();
         let shm: WlShm = globals.bind(&qh, 1..=1, ()).unwrap();
+        let xdg_decoration_manager: ZxdgDecorationManagerV1 = globals.bind(&qh, 1..=1, ()).unwrap();
 
         Ok(Rc::new(Self {
             connection,
             globals,
             compositor,
             shm,
+            xdg_decoration_manager
         }))
     }
 
