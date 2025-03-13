c2f67347fdb9fec112de118f47178fc4bcea6815
Vladimir Zarypov <krre31@gmail.com>
2025-03-13 06:53:18 +0200

    Add `windows::Application::hinstance:` member

--- src/platform/windows/application.rs
+++ src/platform/windows/application.rs
@@ -1,14 +1,21 @@
+use windows::Win32::Foundation::HMODULE;
+use windows::Win32::System::LibraryLoader::*;
+
 use std::any::Any;
 use std::rc::Rc;
 
 use crate::core::Result;
 use crate::platform::PlatformApplication;
 
-pub struct Application {}
+pub struct Application {
+    pub(crate) hinstance: HMODULE
+}
 
 impl Application {
     pub fn new() -> Result<Rc<dyn PlatformApplication>> {
-        Ok(Rc::new(Self {}))
+        Ok(Rc::new(Self {
+            hinstance: unsafe { GetModuleHandleW(None) }?
+        }))
     }
 }
 
