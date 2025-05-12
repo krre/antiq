66979069c3cef8891f19e43f104456cf8c852e32
Vladimir Zarypov <krre31@gmail.com>
2025-05-12 08:54:52 +0300

    Remove `unwrap()` from `create_window` test

--- tests/window.rs
+++ tests/window.rs
@@ -1,16 +1,21 @@
 use antiq::application::Application;
+use antiq::core::Result;
 use antiq::window::Window;
 
 #[test]
-fn create_window() {
-    let app = Application::new().unwrap();
+fn create_window() -> Result<()> {
+    let app = Application::new()?;
 
     let title = "Test Window".to_string();
 
-    let window = Window::new(&app).unwrap().upgrade().unwrap();
+    let window = Window::new(&app)?
+        .upgrade()
+        .ok_or("Window weak reference is invalid")?;
     {
         window.borrow_mut().set_title(&title);
     }
 
     assert_eq!(window.borrow().title(), title);
+
+    Ok(())
 }
