314bba4533923ee77c0e71acadbd588efbffc080
Vladimir Zarypov <krre31@gmail.com>
2025-05-14 08:31:38 +0300

    Add view example

---
+++ examples/view.rs
@@ -0,0 +1,30 @@
+use antiq::{
+    application::Application,
+    core::Result,
+    ui::{layout::fill::Fill, widget::view::View},
+    window::Window,
+};
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let app = Application::new()?;
+    let window = Window::new(&app)?;
+
+    {
+        let w = window.upgrade().unwrap();
+        let mut w = w.borrow_mut();
+        w.set_title("View Example");
+        w.set_visible(true);
+
+        let view = View::new();
+        let mut fill_layout = Fill::new();
+        fill_layout.set_widget(Box::new(view));
+
+        w.set_layout(Box::new(fill_layout));
+    }
+
+    app.run()?;
+
+    Ok(())
+}
