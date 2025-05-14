46f3d9cfffb03cfda7a72a60ce4d89f7300faa62
Vladimir Zarypov <krre31@gmail.com>
2025-05-14 08:08:10 +0300

    Remove `EmptyWidget` struct

--- src/ui/widget/mod.rs
+++ src/ui/widget/mod.rs
@@ -1,18 +1,3 @@
-pub mod rectangle;
-pub use rectangle::Rectangle;
-
 pub trait Widget {
     fn build(&self);
-}
-
-pub struct EmptyWidget {}
-
-impl EmptyWidget {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
-impl Widget for EmptyWidget {
-    fn build(&self) {}
 }
