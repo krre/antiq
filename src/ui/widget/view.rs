24b49d940cf93820feb086e12ef2dd131ea5956a
Vladimir Zarypov <krre31@gmail.com>
2025-05-16 06:12:44 +0300

    Addd scene to view

--- src/ui/widget/view.rs
+++ src/ui/widget/view.rs
@@ -1,10 +1,26 @@
+use std::rc::Rc;
+
+use crate::ui::scene::Scene;
+
 use super::Widget;
 
-pub struct View {}
+pub struct View {
+    scene: Rc<Scene>,
+}
 
 impl View {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            scene: Rc::new(Scene::new()),
+        }
+    }
+
+    pub fn set_scene(&mut self, scene: Rc<Scene>) {
+        self.scene = scene;
+    }
+
+    pub fn scene(&self) -> Rc<Scene> {
+        self.scene.clone()
     }
 }
 
