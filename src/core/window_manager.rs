eeef1c9e5360603706345f1167dc89d403cf82e5
Vladimir Zarypov <krre31@gmail.com>
2025-05-17 14:28:27 +0300

    Remove crate visibility from `WindowManager` methods

--- src/core/window_manager.rs
+++ src/core/window_manager.rs
@@ -9,43 +9,43 @@ pub(crate) struct WindowManager {
 }
 
 impl WindowManager {
-    pub(crate) fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             windows: HashMap::new(),
         }
     }
 
-    pub(crate) fn append(&mut self, id: WindowId, window: Rc<RefCell<Window>>) {
+    pub fn append(&mut self, id: WindowId, window: Rc<RefCell<Window>>) {
         #[allow(unused_variables)]
         let window = self.windows.insert(id, window);
     }
 
-    pub(crate) fn remove(&mut self, id: WindowId) {
+    pub fn remove(&mut self, id: WindowId) {
         #[allow(unused_variables)]
         let window = self.windows.remove(&id);
     }
 
-    pub(crate) fn render(&self, id: WindowId) {
+    pub fn render(&self, id: WindowId) {
         self.windows.get(&id).map(|w| w.borrow().render());
     }
 
-    pub(crate) fn resize(&self, id: WindowId, size: Size2D) {
+    pub fn resize(&self, id: WindowId, size: Size2D) {
         self.windows
             .get(&id)
             .map(|w| w.borrow_mut().update_size(size));
     }
 
-    pub(crate) fn ask_resize(&self, id: WindowId, size: Size2D) {
+    pub fn ask_resize(&self, id: WindowId, size: Size2D) {
         self.windows.get(&id).map(|w| w.borrow_mut().set_size(size));
     }
 
-    pub(crate) fn move_to(&self, id: WindowId, pos: Pos2D) {
+    pub fn move_to(&self, id: WindowId, pos: Pos2D) {
         self.windows
             .get(&id)
             .map(|w| w.borrow_mut().update_position(pos));
     }
 
-    pub(crate) fn count(&self) -> usize {
+    pub fn count(&self) -> usize {
         self.windows.len()
     }
 }
