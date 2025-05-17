567d07caf96cc11687c4557a70800730770017b4
Vladimir Zarypov <krre31@gmail.com>
2025-05-17 10:05:08 +0300

    Add widgets container to `Row2D` layout

--- src/ui/d2/layout/row.rs
+++ src/ui/d2/layout/row.rs
@@ -1,12 +1,28 @@
-use crate::ui::layout::Layout;
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
+
+use crate::ui::{d2::widget::Widget2D, layout::Layout};
 
 use super::Layout2D;
 
-pub struct Row2D {}
+pub struct Row2D {
+    widgets: Vec<Rc<RefCell<dyn Widget2D>>>,
+}
 
 impl Row2D {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            widgets: Vec::new(),
+        }
+    }
+
+    pub fn add_widget<T: Widget2D + 'static>(&mut self, widget: T) -> Weak<RefCell<T>> {
+        let rc = Rc::new(RefCell::new(widget));
+        let weak = Rc::downgrade(&rc);
+        self.widgets.push(rc);
+        weak
     }
 }
 
