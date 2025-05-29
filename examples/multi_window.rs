003943d43d709c9540f8546ea258c4d32e2f132a
Vladimir Zarypov <krre31@gmail.com>
2025-05-29 17:42:41 +0300

    Rename title of windows in `multi_window` example

--- examples/multi_window.rs
+++ examples/multi_window.rs
@@ -17,7 +17,7 @@ fn main() -> Result<()> {
 
     {
         let w = window_1.upgrade_or_err()?;
-        w.borrow_mut().set_title("Multi Window Example 1");
+        w.borrow_mut().set_title("Multi Window 1 Example");
     }
 
     let window_2 = Window::new(&app)?;
@@ -25,7 +25,7 @@ fn main() -> Result<()> {
     {
         let w = window_2.upgrade_or_err()?;
         let mut w = w.borrow_mut();
-        w.set_title("Multi Window Example 2");
+        w.set_title("Multi Window 2 Example");
         w.set_position(Pos2D::new(500, 200));
         w.set_size(Size2D::new(300, 300));
         w.set_color(Color::new(1.0, 0.0, 0.0));
