e10760ec252f4fa1bcec19a0f5f3731904ad170f
Vladimir Zarypov <krre31@gmail.com>
2025-07-22 15:35:37 +0300

    Remove tuple from `Window` struct

--- src/core/window.rs
+++ src/core/window.rs
@@ -1,17 +1,19 @@
 use crate::ui::d2::geometry::Size2D;
 
-pub struct Window(web_sys::Window);
+pub struct Window {
+    inner: web_sys::Window,
+}
 
 impl Window {
     pub fn new() -> Self {
         Self {
-            0: web_sys::window().unwrap(),
+            inner: web_sys::window().unwrap(),
         }
     }
 
     pub fn size(&self) -> Size2D {
-        let width = self.0.inner_width().ok().unwrap().as_f64().unwrap() as u32;
-        let height = self.0.inner_height().ok().unwrap().as_f64().unwrap() as u32;
+        let width = self.inner.inner_width().ok().unwrap().as_f64().unwrap() as u32;
+        let height = self.inner.inner_height().ok().unwrap().as_f64().unwrap() as u32;
         Size2D::new(width, height)
     }
 }
