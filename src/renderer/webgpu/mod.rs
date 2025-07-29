20737219b66422c2b88194aab60536181cf5419f
Vladimir Zarypov <krre31@gmail.com>
2025-07-29 06:27:08 +0300

    Fix module name

--- src/renderer/webgpu/mod.rs
+++ src/renderer/webgpu/mod.rs
@@ -1,9 +1,9 @@
 pub mod adapter;
-pub mod canval_context;
+pub mod canvas_context;
 pub mod device;
 pub mod gpu;
 
 pub use adapter::Adapter;
-pub use canval_context::CanvasContext;
+pub use canvas_context::CanvasContext;
 pub use device::Device;
 pub use gpu::Gpu;
