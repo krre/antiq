d56b7cf85bd9de24ee040c0e9cbfc2e11c819094
Vladimir Zarypov <krre31@gmail.com>
2025-08-02 12:32:32 +0300

    Remove closures from `EventDispatcher` state

--- src/event/event_dispatcher.rs
+++ src/event/event_dispatcher.rs
@@ -2,33 +2,27 @@ use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::Closure;
 use web_sys::{MouseEvent, Window};
 
-pub(crate) struct EventDispatcher {
-    resize_closure: Closure<dyn FnMut()>,
-    mouse_move_closure: Closure<dyn FnMut(MouseEvent)>,
-}
+pub(crate) struct EventDispatcher {}
 
 impl EventDispatcher {
     pub(crate) fn new(window: &Window) -> Self {
         // resize
         let resize_closure = Closure::<dyn FnMut()>::new(move || {});
-
         window
             .add_event_listener_with_callback("resize", resize_closure.as_ref().unchecked_ref())
             .unwrap();
+        resize_closure.forget();
 
         // mousemove
         let mouse_move_closure = Closure::<dyn FnMut(MouseEvent)>::new(move |event: MouseEvent| {});
-
         window
             .add_event_listener_with_callback(
                 "mousemove",
                 mouse_move_closure.as_ref().unchecked_ref(),
             )
             .unwrap();
+        mouse_move_closure.forget();
 
-        Self {
-            resize_closure,
-            mouse_move_closure,
-        }
+        Self {}
     }
 }
