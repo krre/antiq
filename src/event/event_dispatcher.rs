120a14aa9e494b5c90f023ef3bef5f0c148f01bf
Vladimir Zarypov <krre31@gmail.com>
2025-09-01 09:33:44 +0300

    Use `gloo::events::EventListener` type for web event handling

--- src/event/event_dispatcher.rs
+++ src/event/event_dispatcher.rs
@@ -1,38 +1,35 @@
 use std::rc::Rc;
 
+use gloo::events::EventListener;
 use wasm_bindgen::JsCast;
-use wasm_bindgen::prelude::Closure;
 
 use crate::event::EventHandler;
+use crate::Window;
 
 pub(crate) struct EventDispatcher {
     handlers: Vec<Rc<dyn EventHandler>>,
+    _listeners: Vec<EventListener>,
 }
 
 impl EventDispatcher {
     pub(crate) fn new() -> Self {
         let window = gloo::utils::window();
 
-        // resize
-        let resize_closure = Closure::<dyn FnMut()>::new(move || {});
+        let resize_listener = EventListener::new(&window, "resize", move |_| {
+            let size = Window::size();
+            gloo::console::log!("resize", size.width(), size.height());
+        });
 
-        window
-            .add_event_listener_with_callback("resize", resize_closure.as_ref().unchecked_ref())
-            .unwrap();
-        resize_closure.forget();
-
-        // mousemove
-        let mouse_move_closure = Closure::<dyn FnMut(web_sys::MouseEvent)>::new(move |event: web_sys::MouseEvent| {});
-        window
-            .add_event_listener_with_callback(
-                "mousemove",
-                mouse_move_closure.as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        mouse_move_closure.forget();
+        let mouse_move_listener = EventListener::new(&window, "mousemove", move |event| {
+            let event = event.dyn_ref::<web_sys::MouseEvent>().unwrap();
+            let x = event.client_x();
+            let y = event.client_y();
+            gloo::console::log!("mousemove", x, y);
+        });
 
         Self {
             handlers: Vec::new(),
+            _listeners: vec![resize_listener, mouse_move_listener]
         }
     }
 
