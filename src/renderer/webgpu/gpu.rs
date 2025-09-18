05803445606b8637ed791d80d90170695ee7ca9b
Vladimir Zarypov <krre31@gmail.com>
2025-09-18 05:39:06 +0300

    Remove underscore from `Gpu::_inner` member

--- src/renderer/webgpu/gpu.rs
+++ src/renderer/webgpu/gpu.rs
@@ -4,16 +4,16 @@ use wasm_bindgen_futures::JsFuture;
 use super::Adapter;
 
 pub struct Gpu {
-    _inner: web_sys::Gpu,
+    inner: web_sys::Gpu,
 }
 
 impl Gpu {
     pub(crate) fn new(gpu: web_sys::Gpu) -> Self {
-        Self { _inner: gpu }
+        Self { inner: gpu }
     }
 
     pub async fn request_adapter(&self) -> Result<Adapter, JsValue> {
-        let adapter = JsFuture::from(self._inner.request_adapter())
+        let adapter = JsFuture::from(self.inner.request_adapter())
             .await?
             .dyn_into::<web_sys::GpuAdapter>()?;
         Ok(Adapter::new(adapter))
