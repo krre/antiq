a8ab21ba5a18b8c25eecbfd3a9fca91ffedadf52
Vladimir Zarypov <krre31@gmail.com>
2025-10-03 13:16:23 +0300

    Add configuring canvas context

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::JsValue;
+use web_sys::{GpuCanvasConfiguration, GpuTextureFormat};
 
 use crate::{
     renderer::webgpu::{Adapter, CanvasContext, Device, Gpu},
@@ -18,6 +19,10 @@ impl Renderer {
     pub(crate) async fn new(gpu: Gpu, context: CanvasContext) -> Result<Self, JsValue> {
         let adapter = gpu.request_adapter().await?;
         let device = adapter.request_device().await?;
+
+        let configuration =
+            GpuCanvasConfiguration::new(device.into_inner(), GpuTextureFormat::Rgba8unorm);
+        context.configure(configuration)?;
 
         Ok(Self {
             gpu,
