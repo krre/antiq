342f6367feba2ad439678488952bf0954ee15da0
Vladimir Zarypov <krre31@gmail.com>
2025-10-03 10:24:34 +0300

    Add `CanvasContext::configure()` method

--- src/renderer/webgpu/canvas_context.rs
+++ src/renderer/webgpu/canvas_context.rs
@@ -1,11 +1,19 @@
+use wasm_bindgen::JsValue;
+use web_sys::GpuCanvasConfiguration;
+
 pub struct CanvasContext {
-    _inner: web_sys::GpuCanvasContext,
+    inner: web_sys::GpuCanvasContext,
 }
 
 impl CanvasContext {
     pub(crate) fn new(gpu_canvas_context: web_sys::GpuCanvasContext) -> Self {
         Self {
-            _inner: gpu_canvas_context,
+            inner: gpu_canvas_context,
         }
+    }
+
+    pub fn configure(&self, configuration: GpuCanvasConfiguration) -> Result<(), JsValue> {
+        self.inner.configure(&configuration)?;
+        Ok(())
     }
 }
