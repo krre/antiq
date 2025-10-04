92913c899f9798da63194ec490aeb0348785b41a
Vladimir Zarypov <krre31@gmail.com>
2025-10-04 13:23:18 +0300

    Add background color to `Ui3d`

--- src/ui/ui3d.rs
+++ src/ui/ui3d.rs
@@ -1,5 +1,23 @@
-use crate::event::EventHandler;
+use crate::{event::EventHandler, ui::Color};
 
-pub struct Ui3d {}
+pub struct Ui3d {
+    backgroud_color: Color,
+}
+
+impl Ui3d {
+    pub fn new() -> Self {
+        Self {
+            backgroud_color: Color::new(0.0, 0.0, 1.1),
+        }
+    }
+
+    pub fn set_backgroud_color(&mut self, color: &Color) {
+        self.backgroud_color = color.clone();
+    }
+
+    pub fn backgroud_color(&self) -> &Color {
+        &self.backgroud_color
+    }
+}
 
 impl EventHandler for Ui3d {}
