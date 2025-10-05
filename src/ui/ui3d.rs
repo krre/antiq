48d564e0ea41798b3580ff60e2d5f4352bfad7c9
Vladimir Zarypov <krre31@gmail.com>
2025-10-05 17:26:02 +0300

    Add layout to `Ui3d`

--- src/ui/ui3d.rs
+++ src/ui/ui3d.rs
@@ -1,13 +1,21 @@
-use crate::{event::EventHandler, ui::Color};
+use crate::{
+    event::EventHandler,
+    ui::{
+        Color,
+        d2::layout::{Fill2D, Layout2D},
+    },
+};
 
 pub struct Ui3d {
     backgroud_color: Color,
+    layout: Box<dyn Layout2D>,
 }
 
 impl Ui3d {
     pub fn new() -> Self {
         Self {
             backgroud_color: Color::new(0.0, 0.0, 1.1),
+            layout: Box::new(Fill2D::new()),
         }
     }
 
@@ -17,6 +25,14 @@ impl Ui3d {
 
     pub fn backgroud_color(&self) -> &Color {
         &self.backgroud_color
+    }
+
+    pub fn set_layout(&mut self, layout: Box<dyn Layout2D>) {
+        self.layout = layout;
+    }
+
+    pub fn layout(&self) -> &Box<dyn Layout2D> {
+        &self.layout
     }
 }
 
